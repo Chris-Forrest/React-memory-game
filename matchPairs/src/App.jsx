ce551a0186d1743614cd7ea7fae49a87a2e24c32
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-05-12 09:28:35 -0400

    Update App.jsx
    added basic functions to start game and turn card and added forms

--- matchPairs/src/App.jsx
+++ matchPairs/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Form from './components/Form';
+import MemoryCard from './components/MemoryCard';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isGameOn, setIsGameOn] = useState(false);
+
+  function startGame(e) {
+    e.preventDefault()
+    setIsGameOn(true)
+  };
+
+  function turnCard() {
+    console.log("Memory card clicked")
+  };
+  
 
   return (
     <main>
-      
+      <h1>Memory</h1>
+      {!isGameOn && <Form handleSubmit={startGame} />}
+      {isGameOn && <MemoryCard handleClick={turnCard} />}
     </main>
   )
 }
