e6ab7f3dfcfba0b2aeec54edaaa93059783d2745
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-05-26 11:33:07 -0400

    Update EmojiButton.jsx
    type prevented hoover effects and styling changes

--- matchPairs/src/components/EmojiButton.jsx
+++ matchPairs/src/components/EmojiButton.jsx
@@ -1,15 +1,21 @@
-export default function EmojiButton ({ content, handleClick, selectedCardEntry, matchedCardEntry}) {
-
+export default function EmojiButton({
+    content,
+    handleClick,
+    selectedCardEntry,
+    matchedCardEntry
+}) {
+    
     const btnContent = selectedCardEntry || matchedCardEntry ? content : "?"
-
-    const btnStyle = matchedCardEntry ? "btn--emoji__back--matched":
-        selectedCardEntry ? "btn--emoji__back--matched":
-        "btn--emoji__font"
-
+    
+    const btnStyle =
+        matchedCardEntry ? "btn--emoji__back--matched" :
+        selectedCardEntry ? "btn--emoji__back--selected" :
+        "btn--emoji__front"
+ 
     return (
         <button
-          className={`btn btn--emoji ${btnStyle}`}
-          onClick={handleClick}
+            className={`btn btn--emoji ${btnStyle}`}
+            onClick={handleClick}
         >
             {btnContent}
         </button>
