eab37d0ec474bc244b07eb256586d193b2b1a8be
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-06-02 10:28:17 -0400

    Update AssistiveTechInfo.jsx
    completing screen reader helper component

--- matchPairs/src/components/AssistiveTechInfo.jsx
+++ matchPairs/src/components/AssistiveTechInfo.jsx
@@ -3,10 +3,10 @@ export default function AssistiveTechInfo({emojisData, matchedCards}){
 
 
     return (
-        <>
+        <section>
         <h2>Game Status</h2>
-        <p>`Number of matched pairs: ${}`</p>
-        <p>`Number of cards left to match: ${}`</p>
-        </>
+        <p>Number of matched pairs: ${matchedCards.length / 2}</p>
+        <p>Number of cards left to match: ${emojisData.length - matchedCards.length}</p>
+        </section>
     )
 };
