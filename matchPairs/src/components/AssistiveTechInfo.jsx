7806d90cd9e87ed17141ddc8c52b7f77260ed38a
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-06-02 10:33:13 -0400

    Update AssistiveTechInfo.jsx
    correction

--- matchPairs/src/components/AssistiveTechInfo.jsx
+++ matchPairs/src/components/AssistiveTechInfo.jsx
@@ -5,8 +5,8 @@ export default function AssistiveTechInfo({emojisData, matchedCards}){
     return (
         <section>
         <h2>Game Status</h2>
-        <p>Number of matched pairs: ${matchedCards.length / 2}</p>
-        <p>Number of cards left to match: ${emojisData.length - matchedCards.length}</p>
+        <p>Number of matched pairs: {matchedCards.length / 2}</p>
+        <p>Number of cards left to match: {emojisData.length - matchedCards.length}</p>
         </section>
     )
 };
