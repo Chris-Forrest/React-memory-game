d505596509ade63e7e79a0e898860362bfacd3d9
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-06-02 10:23:50 -0400

    Create AssistiveTechInfo.jsx
    made a component to give pertinent game data to screen readers

---
+++ matchPairs/src/components/AssistiveTechInfo.jsx
@@ -0,0 +1,12 @@
+
+export default function AssistiveTechInfo({emojisData, matchedCards}){
+
+
+    return (
+        <>
+        <h2>Game Status</h2>
+        <p>`Number of matched pairs: ${}`</p>
+        <p>`Number of cards left to match: ${}`</p>
+        </>
+    )
+};
