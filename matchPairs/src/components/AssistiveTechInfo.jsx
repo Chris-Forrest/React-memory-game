29f92e759659abc024b3f2e06d9a0cda9a697a4d
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2025-06-05 09:03:17 -0400

    Update AssistiveTechInfo.jsx
    added sr-only so that it's only screen reader accessible and there isn't a visual representation

--- matchPairs/src/components/AssistiveTechInfo.jsx
+++ matchPairs/src/components/AssistiveTechInfo.jsx
@@ -3,7 +3,7 @@ export default function AssistiveTechInfo({emojisData, matchedCards}){
 
 
     return (
-        <section>
+        <section className="sr-only">
         <h2>Game Status</h2>
         <p>Number of matched pairs: {matchedCards.length / 2}</p>
         <p>Number of cards left to match: {emojisData.length - matchedCards.length}</p>
